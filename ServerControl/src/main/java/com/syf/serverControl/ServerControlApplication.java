3f35cdfa460dd291effa188655a7ec7614799726
邵云飞 <s19153491582@163.com>
2024-10-30 19:35:37 +0800

    服务器监控项目塔建

---
+++ ServerControl/src/main/java/com/syf/serverControl/ServerControlApplication.java
@@ -0,0 +1,11 @@
+package com.syf.serverControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerControlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerControlApplication.class, args);
+    }
+}
